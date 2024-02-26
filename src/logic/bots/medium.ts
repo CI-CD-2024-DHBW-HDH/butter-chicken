52746c9520f8a3474b3f560de93dfe17404c1683
Samuel Kraut <skraut@artiso.com>
2024-02-26 10:36:17 +0100

    add medium function

--- src/logic/bots/medium.ts
+++ src/logic/bots/medium.ts
@@ -1,12 +1,29 @@
-import { getBlanks, invertPlayer, type Field } from "../game";
+import { getBlanks, invertPlayer, Field } from "../game";
 import { randomMove, winningMove } from "./bot";
 
-// the medium bot plays a wiining move, if it can
+// the medium bot plays a wining move, if it can
 // or blocks the opponent from winning
 // or plays the center field if it can
 // otherwise it plays a random move
 export function mediumMove(board: Field[], own: Field): number {
-  return -1
+  var nextSelfWinningMove : number = winningMove(board,own);
+  if(nextSelfWinningMove !== -1){
+    
+    return nextSelfWinningMove;
+  }
+
+  var nextOpponentWinningMove : number = winningMove(board,own == Field.PLAYER1 ? Field.PLAYER2 : Field.PLAYER1);
+  if(nextOpponentWinningMove !== -1){
+    return nextOpponentWinningMove;
+  }
+
+  if(board[4] === Field.EMPTY){
+    return 4;
+  }
+  var blanks : number[] = getBlanks(board);
+  var randomPosition : number = randomMove(blanks.length);
+
+  return randomPosition
 }
 
 // this bot just tries to block a win
