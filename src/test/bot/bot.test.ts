c095108b7c2c0f7267bf488ff05aaa5e9d18051a
Philipp Erath <philipp.erath@bs-sd.de>
2024-02-26 13:44:08 +0100

    fixed

--- src/test/bot/bot.test.ts
+++ src/test/bot/bot.test.ts
@@ -13,9 +13,20 @@ test("test WinningMove", () => {
     [0, 0, 1, 0, 1, 0, 0, 0, 0],
   ];
 
-  for (let item of winCombinations) {
-    let tori = newBoard();
-    tori = item;
-    expect(winningMove(tori, Field.PLAYER1)).not.toBe(-1);
-  }
-});
+test("test WinningMove",() =>{
+    const winCombinations: number[][] = [
+        [1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 1, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 1, 1],
+            [1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 1],
+            [1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 0, 0, 0]]
+
+    for(const item of winCombinations){
+        let tori = newBoard()
+        tori = item
+        expect(winningMove(tori,Field.PLAYER1)).not.toBe(-1)
+    }
+})
