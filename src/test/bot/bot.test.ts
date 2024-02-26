621cfc7fcb371cc062a27604c99428a622ed58c6
Samuel Kraut <skraut@artiso.com>
2024-02-26 13:40:35 +0100

    .

--- src/test/bot/bot.test.ts
+++ src/test/bot/bot.test.ts
@@ -1,21 +1,21 @@
-import {winningMove} from "../../logic/bots/bot";
-import {Field, newBoard} from "../../logic/game";
+import { winningMove } from "../../logic/bots/bot";
+import { Field, newBoard } from "../../logic/game";
 
+test("test WinningMove", () => {
+  const winCombinations: number[][] = [
+    [1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 0, 0, 0],
+  ];
 
-test("test WinningMove",() =>{
-    const winCombinations: number[][] = [
-        [1, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 1],
-            [1, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0]]
-
-    for(let item of winCombinations){
-        let tori = newBoard()
-        tori = item
-        expect(winningMove(tori,Field.PLAYER1)).not.toBe(-1)
-    }
-})
+  for (let item of winCombinations) {
+    let tori = newBoard();
+    tori = item;
+    expect(winningMove(tori, Field.PLAYER1)).not.toBe(-1);
+  }
+});
