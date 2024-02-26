d5478c108cb36292d9f95dc6f7023a99f4df91b8
Philipp Erath <philipp.erath@bs-sd.de>
2024-02-26 13:49:14 +0100

    applied prettier rules

--- src/test/bot/bot.test.ts
+++ src/test/bot/bot.test.ts
@@ -1,17 +1,24 @@
 import { winningMove } from "../../logic/bots/bot";
 import { Field, newBoard } from "../../logic/game";
 
+test("test WinningMove", () => {
+  const winCombinations: number[][] = [
+    [1, 1, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 0, 0, 0],
+  ];
 
-test("test WinningMove",() =>{
-    const winCombinations: number[][] = [
-        [1, 1, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 1, 1, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 1, 1],
-            [1, 0, 0, 1, 0, 0, 0, 0, 0],
-            [0, 1, 0, 0, 1, 0, 0, 0, 0],
-            [0, 0, 1, 0, 0, 0, 0, 0, 1],
-            [1, 0, 0, 0, 0, 0, 0, 0, 1],
-    [0, 0, 1, 0, 1, 0, 0, 0, 0]]
+  for (const item of winCombinations) {
+    let tori = newBoard();
+    tori = item;
+    expect(winningMove(tori, Field.PLAYER1)).not.toBe(-1);
+  }
+});
 
     for(const item of winCombinations){
         let tori = newBoard()
@@ -21,3 +28,4 @@ test("test WinningMove",() =>{
 })
 
 export {}
+export {};
